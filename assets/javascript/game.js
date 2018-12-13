c7e0432b4d9eb5fd71b3a68965f831cdedee9757
Reese Fairbanks <reese@reeses-mbp.attlocal.net>
2018-12-12 20:20:36 -0500

    Updated Homework Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,8 @@
                 Display losses
                 Game over, reset
             Else user picks a new letter and guesses allowed go down
-*/ 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+*/
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 var userGuess = "";
 var wins = 0;
@@ -24,45 +24,47 @@ var guessedLetters = [];
 var guessesLeft = 9;
 
 // Capture users choice
-    document.onkeyup = function(event) {
-        var userGuess = event.key.toLowerCase();
-        console.log(userGuess);        
-        // Computer picks random letter
-        console.log(computerChoice);
-        if (alphabet.indexOf(userGuess) > -1) {
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    console.log(computerChoice);
+    // Computer picks random letter
+    if (alphabet.indexOf(userGuess) > -1) {
 
         //Compare user guess to computer guess
-            if (userGuess===computerChoice) {
-                //Wins increase
-                wins++;
-                alert("Wins: " + wins);
-                // Computer chooses another letter
-                //computerChoice;
-                //Guessed letters resets
-                guessedLetters = [];
-                //Guesses lrft resets
-                guessesLeft = 9;
+        if (userGuess === computerChoice) {
+            //Wins increase
+            wins++;
+            // Computer chooses another letter
+            computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+            //Guessed letters resets
+            guessedLetters = [];
+            //Guesses left resets
+            guessesLeft = 9;
 
             // If guesses do not match, decrease guesses left and show guessedletters
-            }
-            if (userGuess != computerChoice) {
-                guessesLeft--;
-                guessedLetters.push(userGuess);
-            }
-            // If user runs out of guesses
-            if (guessesLeft === 0) {
-                guessesLeft = 9;
-                losses++;
-                guessedLetters = [];
-            }
-               
+        }
+        // If user runs out of guesses
+        else if (guessesLeft === 0) {
+            guessesLeft = 9;
+            losses++;
+            guessedLetters = [];
+        }
+        else {
+            guessesLeft--;
+            guessedLetters.push(userGuess);
+        }
+
+
         // Write to HTML to display game score
-        var HTML = "<p>Wins: " + wins + "</p>" +
+        var HTML =
+            "<h1>The Psychic Game</h1>" +
+            "<p>Guess what letter I'm thinking of</p>" +
+            "<p>Wins: " + wins + "</p>" +
             "<p>Loses: " + losses + "</P>" +
             "<p>Guesses Left: " + guessesLeft + "</p>" +
-            "<p>Your Guesses: " + userGuess + "</p>";
-        
-        //document.querySelector("#game").innerHTML = html;
-        }
-    };
-   
+            "<p>Your Guesses: " + guessedLetters + "</p>";
+
+        document.querySelector("#game").innerHTML = HTML;
+    }
+};
+
